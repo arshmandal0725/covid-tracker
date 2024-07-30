266146caf8d3158506099e262f462a3f2157cc85
arshmandal0725 <arshmandal07@gmail.com>
2024-07-31 00:13:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React from 'react'
+import Covid from './components/covid'
+
+
+const App = () => {
+  return (
+    <>
+      <Covid/>
+    </>
+  )
+}
+
+export default App
